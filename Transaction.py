d6a1e949c69591f56b7d201a9669da936440d223
ThomasRUdeS <rayt8317@usherbrooke.ca>
2026-04-21 12:33:57 -0400

    Modifications sur l'affichage des valeurs dans le terminal. Premier ajout: les valeurs en argent sont  affichée avec deux chiffres après la virgule au moyen du formatage des string fourni par Python. Deuxième ajout: Lorsque le solde devient négatif il devient rouge au moyen du code ANSI des couleurs. Troisième ajout: Affichage de l'historique des dépenses et des revenus avec le temps auxquel il a été effectué. J'ai utilisé time.localtime() qui retourne un tuple ayant les valeurs du temps actuel. Je doute cependant que si on entre deux valeurs identiques le code ne vachanger que le temps et n'ajoutera pas un nouvel élément dans le dictionnaire. DOnc, à tester/modifier

--- Transaction.py
+++ Transaction.py
@@ -2,11 +2,12 @@
  
 """
  Projet : Finances-Pro-Max
- Autheur : Thomas Raymond
+ Author : Thomas Raymond
  Author : Félix Roussin 
- Date : 20 avril 2026
+ Date : 21 avril 2026
  
- Description : Fonction qui ajoute des dépenses et des revenus
+Description : Fonction utilisable à partir du terminal qui permet à l'utilisateur de rentrer des dépenses et des revenus et d'enregistrer
+son solde.
  
 """
 import array as arr
@@ -30,7 +31,7 @@ class Transaction:
         return self.transaction
         
      def get_solde(self):
-         solde =0
+         solde = 0
          for montant in self.transaction:
              solde += montant
          return solde
@@ -65,25 +66,52 @@ class Transaction:
              print("Transaction non valide.")
              t.sleep(2)
      def __str__(self):
-         return f"Votre solde est de : {self.get_solde()}$ \nVoulez-vous entrer une dépot ou un retrait? (d/r)\nQuitter: (q)"
- 
+        solde=self.get_solde()
+        if solde < 0:
+            return f"Votre solde est de : {"\033[91m"}{solde:.2f}{"\033[0m"}$ \nVoulez-vous entrer une dépot ou un retrait? (d/r)\nQuitter: (q)"
+        return f"Votre solde est de : {solde:.2f}$ \nVoulez-vous entrer une dépot ou un retrait? (d/r)\nQuitter: (q)"
+     
 if __name__ == "__main__":
-     solde = 0
-     transaction=Transaction()
-     while(True):
-         screen_clear()
-         print(transaction.get_historique_depot())
-         print(transaction.get_historique_retrait())
-         print(transaction)
-         choix = input("Entrez votre choix (d/r/q) : ")
-         if choix == "d":
-             transaction.set_transaction(input("Entrez votre dépot en dollars CAD : ",).replace(",","."),choix)
-         elif choix == "r":
-             transaction.set_transaction(input("Entrez votre retrait en dollars CAD : ").replace(",","."),choix)
-         elif choix == "q":
-             screen_clear()
-             print("Votre solde est de : ",transaction.get_solde(),"$")
-             break
+    solde = 0
+    transaction=Transaction()
+    historique_depot={}
+    past_lenght_liste_depot=0
+    historique_retrait={}
+    past_lenght_liste_retrait=0
+    while(True):
+        screen_clear()
+        liste_retrait=transaction.get_historique_retrait()
+        liste_depot=transaction.get_historique_depot()
+        if len(liste_depot)!=0:
+            if len(liste_depot)>past_lenght_liste_depot:
+                historique_depot[liste_depot[len(liste_depot)-1]]=f"{t.localtime()[2]}/{t.localtime()[1]:02.0f}/{t.localtime()[0]:02.0f} à {t.localtime()[3]:02.0f}:{t.localtime()[4]:02.0f}:{t.localtime()[5]:02.0f}"
+                past_lenght_liste_depot+=1
+            print("Historique des dépôts:")
+            for element in historique_depot:
+                print(f"{element:.2f}: {historique_depot[element]}")
+            print()
+        if len(liste_retrait)!=0:
+            if len(liste_retrait)>past_lenght_liste_retrait:
+                historique_retrait[liste_retrait[len(liste_retrait)-1]]=f"{t.localtime()[2]}/{t.localtime()[1]:02.0f}/{t.localtime()[0]:02.0f} à {t.localtime()[3]:02.0f}:{t.localtime()[4]:02.0f}:{t.localtime()[5]:02.0f}"
+                past_lenght_liste_retrait+=1
+            print("Historique des retraits:")
+            for element in historique_retrait:
+                print(f"{element:.2f}: {historique_retrait[element]}")
+            print()
+        print(transaction)
+        choix = input("Entrez votre choix (d/r/q) : ")
+        if choix == "d":
+            transaction.set_transaction(input("Entrez votre dépot en dollars CAD : ",).replace(",","."),choix)
+        elif choix == "r":
+            transaction.set_transaction(input("Entrez votre retrait en dollars CAD : ").replace(",","."),choix)
+        elif choix == "q":
+            screen_clear()
+            solde=transaction.get_solde()
+            if solde<0:
+                print(f"Votre solde est de: {"\033[91m"}{solde:.2f}{"\033[0m"} $")
+            else:
+                print(f"Votre solde est de: {solde:.2f}$")
+            break
          
              
  
